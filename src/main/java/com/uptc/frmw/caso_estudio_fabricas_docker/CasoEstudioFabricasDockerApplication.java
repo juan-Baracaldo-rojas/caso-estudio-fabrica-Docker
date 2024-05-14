b7c6dc3e6ce18dd63d9d8fc7c8dc52a1d6dc4528
Juan Manuel Baracaldo Rojas <87029593+juan-Baracaldo-rojas@users.noreply.github.com>
2024-05-13 21:17:36 -0500

    Creacion aplicacion Springbot

---
+++ src/main/java/com/uptc/frmw/caso_estudio_fabricas_docker/CasoEstudioFabricasDockerApplication.java
@@ -0,0 +1,13 @@
+package com.uptc.frmw.caso_estudio_fabricas_docker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CasoEstudioFabricasDockerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CasoEstudioFabricasDockerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/uptc/frmw/caso_estudio_fabricas_docker/CasoEstudioFabricasDockerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uptc.frmw.caso_estudio_fabricas_docker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CasoEstudioFabricasDockerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

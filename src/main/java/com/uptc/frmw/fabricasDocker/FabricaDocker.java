24dac5cb0d4982d139d8475c8ff42207c7bc778c
AndreaLeal1218 <paola.baron@uptc.edu.co>
2024-05-13 22:31:57 -0500

    Configuracion inicial

--- src/main/java/com/uptc/frmw/fabricasDocker/FabricaDocker.java
+++ src/main/java/com/uptc/frmw/fabricasDocker/FabricaDocker.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CasoEstudioFabricasDockerApplication {
+public class FabricaDocker {
 
     public static void main(String[] args) {
-        SpringApplication.run(CasoEstudioFabricasDockerApplication.class, args);
+        SpringApplication.run(FabricaDocker.class, args);
     }
 
 }
--- src/test/java/com/uptc/frmw/fabricasDocker/FabricaDocker.java
+++ src/test/java/com/uptc/frmw/fabricasDocker/FabricaDocker.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class CasoEstudioFabricasDockerApplicationTests {
+class FabricaDocker {
 
     @Test
     void contextLoads() {
